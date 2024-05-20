b720af410d1fc7569569a090aab292bf73581124
dobam <ckehqja@gmail.com>
2024-05-20 17:55:18 +0900

    .

--- src/test/java/com/sparta/schedule/service/ScheduleServiceTest.java
+++ src/test/java/com/sparta/schedule/service/ScheduleServiceTest.java
@@ -1,14 +1,7 @@
 package com.sparta.schedule.service;
 
-import java.util.List;
-
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
-
-import com.sparta.schedule.controller.dto.ScheduleRequestDto;
-import com.sparta.schedule.controller.dto.ScheduleResponseDto;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -21,16 +14,5 @@ class ScheduleServiceTest {
 
 	@Autowired
 	ScheduleService service;
-
-	@Test
-	@Rollback
-	void test() {
-		ScheduleRequestDto scheduleRequestDto = new ScheduleRequestDto("dd", "dd", "ddd", "ddd");
-		service.createSchedule(scheduleRequestDto);
-		List<ScheduleResponseDto> schedules = service.getSchedules();
-		for (ScheduleResponseDto schedule : schedules) {
-			System.out.println("schedule.getTitle() = " + schedule.getTitle());
-		}
-	}
 
 }
