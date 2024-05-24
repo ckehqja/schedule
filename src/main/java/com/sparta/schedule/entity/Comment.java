2c44be7a9f1c0fac9fb70f3fd9b3766d217950e0
dobam <ckehqja@gmail.com>
2024-05-24 12:47:10 +0900

    숙련주차 1.댓글(다)대 일정(일) 연관 관계 엔티티 생성

---
+++ src/main/java/com/sparta/schedule/entity/Comment.java
@@ -0,0 +1,36 @@
+package com.sparta.schedule.entity;
+
+import com.sparta.schedule.controller.dto.CommentRequestDto;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table
+@NoArgsConstructor
+public class Comment extends Timestamped {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String contents;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "schedule_id")
+	private Schedule schedule;
+	private Long userId;
+
+	public Comment(CommentRequestDto requestDto, Schedule schedule) {
+		this.contents = requestDto.getContents();
+		this.userId = requestDto.getUserId();
+		this.schedule = schedule;
+	}
+}
